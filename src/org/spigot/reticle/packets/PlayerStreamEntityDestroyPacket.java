cf8e47506ac5c1146076dd429c20dc60b4459856
Encorn <encorn73@gmail.com>
2014-10-06 20:22:32 +0200

    Removing in batches

--- src/org/spigot/reticle/packets/PlayerStreamEntityDestroyPacket.java
+++ src/org/spigot/reticle/packets/PlayerStreamEntityDestroyPacket.java
@@ -13,14 +13,27 @@ public class PlayerStreamEntityDestroyPacket extends AbstractPacket {
 
 	public void Write(int[] entityid) throws IOException {
 		if (entityid.length > 0) {
-			byte bytelen=(byte) (entityid.length & 0xff);
-			reader.setOutputStream(4 * entityid.length + 2);
-			reader.writeVarInt(0x13);
-			reader.writeByte(bytelen);
-			for (int ent : entityid) {
-				reader.writeInt(ent);
+			int batch = 100;
+			int done=0;
+			int batches = (int) Math.ceil(entityid.length / batch);
+			for (int i = 0; i < batches; i++) {
+				int batchlen;
+				if(done+batch>entityid.length) {
+					batchlen=entityid.length-(done+batch);
+				} else {
+					batchlen=batch;
+				}
+				byte bytelen = (byte) (batchlen & 0xff);
+				reader.setOutputStream(4 * batchlen + 2);
+				reader.writeVarInt(0x13);
+				reader.writeByte(bytelen);
+				for(int o=done;o<done+batchlen;o++) {
+					int ent=entityid[o];
+					reader.writeInt(ent);
+				}
+				reader.Send();
+				done+=batchlen;
 			}
-			reader.Send();
 		}
 	}
 
